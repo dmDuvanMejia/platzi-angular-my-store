d2814068eb164965ef8d626b683ce5c518b944fe
dquiros <dquiros@ospinternational.com>
2022-01-27 19:55:37 -0500

    Mejora en la modularicación de componentes

---
+++ src/app/components/products/products.component.ts
@@ -0,0 +1,55 @@
+import { Component, OnInit } from '@angular/core';
+import { Product } from '../../models/product.model';
+
+@Component({
+  selector: 'app-products',
+  templateUrl: './products.component.html',
+  styleUrls: ['./products.component.scss']
+})
+export class ProductsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  products: Product[] = [
+    {
+      id: '1',
+      name: 'EL mejor juguete',
+      price: 565,
+      image: './assets/images/toy.jpg'
+    },
+    {
+      id: '2',
+      name: 'Bicicleta casi nueva',
+      price: 356,
+      image: './assets/images/bike.jpg'
+    },
+    {
+      id: '3',
+      name: 'Colleción de albumnes',
+      price: 34,
+      image: './assets/images/album.jpg'
+    },
+    {
+      id: '4',
+      name: 'Mis libros',
+      price: 23,
+      image: './assets/images/books.jpg'
+    },
+    {
+      id: '5',
+      name: 'Otro',
+      price: 245,
+      image: './assets/images/books.jpg'
+    },
+    {
+      id: '6',
+      name: 'Bicicleta casi nueva 2',
+      price: 356,
+      image: './assets/images/bike.jpg'
+    },
+  ];
+
+}
