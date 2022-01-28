ee6976bb88e950ae4c2c61653b73eba5a72ad7b4
dquiros <dquiros@ospinternational.com>
2022-01-28 09:10:22 -0500

    Componentes, servicios, obtener datos de una api

--- src/app/components/products/products.component.ts
+++ src/app/components/products/products.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Product } from '../../models/product.model';
+import { StoreService } from '../../services/store.service';
+import { ProductsService } from '../../services/products.service';
 
 @Component({
   selector: 'app-products',
@@ -8,48 +10,27 @@ import { Product } from '../../models/product.model';
 })
 export class ProductsComponent implements OnInit {
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor( 
+    private storeService: StoreService,
+    private productsService: ProductsService
+   ) { 
+    this.myShoppingCart = this.storeService.getShoppingCart();
   }
 
-  products: Product[] = [
-    {
-      id: '1',
-      name: 'EL mejor juguete',
-      price: 565,
-      image: './assets/images/toy.jpg'
-    },
-    {
-      id: '2',
-      name: 'Bicicleta casi nueva',
-      price: 356,
-      image: './assets/images/bike.jpg'
-    },
-    {
-      id: '3',
-      name: 'Colleción de albumnes',
-      price: 34,
-      image: './assets/images/album.jpg'
-    },
-    {
-      id: '4',
-      name: 'Mis libros',
-      price: 23,
-      image: './assets/images/books.jpg'
-    },
-    {
-      id: '5',
-      name: 'Otro',
-      price: 245,
-      image: './assets/images/books.jpg'
-    },
-    {
-      id: '6',
-      name: 'Bicicleta casi nueva 2',
-      price: 356,
-      image: './assets/images/bike.jpg'
-    },
-  ];
+  ngOnInit(): void {
+    this.productsService.getAllProducts()
+    .subscribe(data => {
+      this.products = data;
+    });
+  }
+
+  myShoppingCart: Product[] = [];
+  total = 0;
+  products: Product[] = [];
+
+  onAddToShoppingCart(product: Product) {
+    this.storeService.addProduct(product);
+    this.total = this.storeService.getTotal();
+  }
 
 }
