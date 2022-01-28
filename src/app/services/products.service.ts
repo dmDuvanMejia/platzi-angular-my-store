ded4ea87d80756e513cec667342b9528232dfbd5
dquiros <dquiros@ospinternational.com>
2022-01-28 17:29:11 -0500

    Reintentar una petición

--- src/app/services/products.service.ts
+++ src/app/services/products.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpParams } from '@angular/common/http';
 
 import { Product, CreateProductDTO, UpdateProductDTO } from './../models/product.model';
+import { retry } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -20,17 +21,24 @@ export class ProductsService {
       params = params.set('limit', limit);
       params = params.set('offset', limit);
     }
-    return this.http.get<Product[]>(this.apiUrl, { params });
+    return this.http.get<Product[]>(this.apiUrl, { params })
+    .pipe(
+      retry(3)
+    );
   }
 
   getProduct(id: string) {
-    return this.http.get<Product>(`${this.apiUrl}/${id}`)
+    return this.http.get<Product>(`${this.apiUrl}/${id}`).pipe(
+      retry(3)
+    );
   }
 
   getProductsByPage(limit: number, offset: number) {
     return this.http.get<Product[]>(`${this.apiUrl}`, {
       params: { limit, offset }
-    })
+    }).pipe(
+      retry(3)
+    );
   }
 
   create(dto: CreateProductDTO) {
