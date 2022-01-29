5bd33d2f13968d9410e3f4cca2ee8f0ce65f62a1
dquiros <dquiros@ospinternational.com>
2022-01-28 22:11:19 -0500

    Creando un contexto a interceptor

--- src/app/services/products.service.ts
+++ src/app/services/products.service.ts
@@ -5,6 +5,7 @@ import { throwError, zip } from 'rxjs';
 
 import { Product, CreateProductDTO, UpdateProductDTO } from './../models/product.model';
 import { environment } from './../../environments/environment';
+import { checkTime } from '../interceptors/time.interceptor';
 
 @Injectable({
   providedIn: 'root'
@@ -23,7 +24,7 @@ export class ProductsService {
       params = params.set('limit', limit);
       params = params.set('offset', limit);
     }
-    return this.http.get<Product[]>(this.apiUrl, { params })
+    return this.http.get<Product[]>(this.apiUrl, { params, context: checkTime() })
     .pipe(
       catchError((error: HttpErrorResponse) => {
         return this.msgErrorHtmlCode(error.status);
